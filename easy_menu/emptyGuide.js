990d7492540323657c713a492981f171a1905822
Angelo <angelo.polyart@gmail.com>
2024-10-01 21:14:24 +0100

    pushed

---
+++ easy_menu/emptyGuide.js
@@ -0,0 +1,13 @@
+export const GUIDES = {
+  example: {
+    clickSound: "sounds/ui/button_click", // Sound to play when buttons are clicked
+    generateBackButton: true,
+    backButton: {
+      text: "Back",
+      image: "",
+      link: "back"
+    },
+    pages: {
+    }
+  }
+};
